3ad3571287676adc7de42ff7170642e217e1a015
angello <angel.gutz.gtiez@gmail.com>
2026-04-21 17:34:19 +0200

    adding logic for visualize triangle wave

--- todo/tremolo_plugin/include/Tremolo/LfoVisualizer.h
+++ todo/tremolo_plugin/include/Tremolo/LfoVisualizer.h
@@ -2,15 +2,27 @@ namespace tremolo {
 class LfoVisualizer : public juce::Component {
 public:
     void resized() override {
-        const int sineEdge = 5;
-        const int sineStartPoint = sineEdge * -1;
+        const auto scale = 0.1f;
+        const int waveEdge = 5;
+        const int waveStartPoint = waveEdge * -1;
         const auto halfHeight = getHeight() / 2;
         const auto amplitude = halfHeight - strokeWidth / 2;
 
-        sine.startNewSubPath(sineStartPoint - sineEdge, amplitude * std::sin(sineStartPoint * 0.1f) + halfHeight);
+        wave.startNewSubPath(waveStartPoint - waveEdge, amplitude * std::sin(waveStartPoint * scale) + halfHeight);
 
-        for(const auto x : std::views::iota(sineStartPoint + 1, getWidth() + sineEdge)) {
-            sine.lineTo(x, amplitude * std::sin(0.1f * x) + halfHeight);
+        for(const auto x : std::views::iota(waveStartPoint + 1, getWidth() + waveEdge)) {
+            float ft, y;
+
+            if(true) {
+                ft = x * scale;
+                y = amplitude * std::sin(x * scale) + halfHeight;
+            }
+            else {
+                ft = x / juce::MathConstants<float>::twoPi * scale;
+                y = (4 * std::abs(ft - std::floor(ft + 0.5f)) - 1.f) * amplitude + halfHeight;
+            }
+
+            wave.lineTo(x, y);
         }
     }
 
@@ -19,7 +31,11 @@ public:
 
     void paint(juce::Graphics& g) override {
         g.setColour(juce::Colours::orange);
-        g.strokePath(sine, juce::PathStrokeType{strokeWidth});
+        //g.strokePath(sine, juce::PathStrokeType{strokeWidth});
+
+        g.strokePath(wave, juce::PathStrokeType{strokeWidth});
+
+
     }
 
 
@@ -33,7 +49,8 @@ public:
 private:    
     float strokeWidth = 4.f;
 
-    juce::Path sine;
+    juce::Path wave;
+
 
 
 }; // LfoVisualizer class
